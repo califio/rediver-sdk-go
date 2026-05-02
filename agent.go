d8067d0a26b9b5e51fc0bdf5e06438375088157c
0xduo <buianhduong9x@gmail.com>
2026-05-02 11:31:04 +0700

    refactor(sdk): extract scanner metadata sync into agent_metadata.go

--- agent.go
+++ agent.go
@@ -470,65 +470,6 @@ func (a *agent) executeCIJob(ctx context.Context, ci *CIContext) error {
 	return nil
 }
 
-// --- Scanner metadata update ---
-
-func (a *agent) syncScannerMetadata(ctx context.Context) {
-	name := a.scanner.Name()
-	req := &agentv1.UpdateScannerRequest{Name: name}
-	var needsUpdate bool
-
-	if dn, ok := a.scanner.(interface{ DisplayName() string }); ok {
-		if displayName := dn.DisplayName(); displayName != "" {
-			req.DisplayName = &displayName
-			needsUpdate = true
-		}
-	}
-
-	if ps, ok := a.scanner.(interface{ ParamsSchema() map[string]interface{} }); ok {
-		if schema := ps.ParamsSchema(); schema != nil {
-			s, err := structpb.NewStruct(schema)
-			if err == nil {
-				req.ParamsSchema = s
-				needsUpdate = true
-			}
-		}
-	} else if ps := a.scanner.Params(); len(ps) > 0 {
-		schema := ParamsToJSONSchema(ps)
-		if schema != nil {
-			s, err := structpb.NewStruct(schema)
-			if err == nil {
-				req.ParamsSchema = s
-				needsUpdate = true
-			}
-		}
-	}
-
-	if types := a.scanner.AssetTypes(); len(types) > 0 {
-		assetTypes := make([]agentv1.AssetType, len(types))
-		for i, t := range types {
-			assetTypes[i] = toProtoAssetType(t)
-		}
-		req.AssetTypes = assetTypes
-		needsUpdate = true
-	}
-
-	if rc, ok := a.scanner.(interface{ SupportsRetest() bool }); ok {
-		if supportsRetest := rc.SupportsRetest(); supportsRetest {
-			req.SupportsRetest = &supportsRetest
-			needsUpdate = true
-		}
-	}
-
-	if !needsUpdate {
-		return
-	}
-	if err := a.client.UpdateScanner(ctx, req); err != nil {
-		a.logger.Warn("failed to sync scanner metadata", "error", err)
-	} else {
-		a.logger.Info("scanner metadata synced")
-	}
-}
-
 // --- Worker pool job wrapper ---
 
 type agentPoolJob struct {
